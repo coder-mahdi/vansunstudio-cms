bfb669cbc6e71e398de7ebe1990f4c28e18e88c8
mahdi-mim <mahdiroozbahani25@gmail.com>
2025-07-14 22:36:17 -0700

    edited content php

--- template-parts/content.php
+++ template-parts/content.php
@@ -2,68 +2,52 @@
 /**
  * Template part for displaying posts
  *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
  * @package vansunstudio-cms
  */
-
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+  <header class="entry-header">
+    <?php
+    if ( is_singular() ) :
+      the_title( '<h1 class="entry-title">', '</h1>' );
+    else :
+      the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+    endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				vansunstudio_cms_posted_on();
-				vansunstudio_cms_posted_by();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+    if ( 'post' === get_post_type() ) : ?>
+      <div class="entry-meta">
+        <?php
+        vansunstudio_cms_posted_on();
+        vansunstudio_cms_posted_by();
+        ?>
+      </div>
+    <?php endif; ?>
+  </header>
 
-	<?php vansunstudio_cms_post_thumbnail(); ?>
+  <?php vansunstudio_cms_post_thumbnail(); ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'vansunstudio-cms' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+  <div class="entry-content">
+    <?php
+    the_content();
 
-		if (get_field('blog_content')) :
-			echo '<div class="acf-blog-body">';
-			echo apply_filters('the_content', get_field('Blog Post'));
-			echo '</div>';
-		  endif;
+    if (function_exists('get_field')) :
+      $acf_content = get_field('blog_post');
+      if ($acf_content && !empty($acf_content)) :
+        echo '<div class="acf-blog-body">';
+        echo apply_filters('the_content', $acf_content);
+        echo '</div>';
+      endif;
+    endif;
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'vansunstudio-cms' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+    wp_link_pages([
+      'before' => '<div class="page-links">' . esc_html__('Pages:', 'vansunstudio-cms'),
+      'after'  => '</div>',
+    ]);
+    ?>
+  </div>
 
-	<footer class="entry-footer">
-		<?php vansunstudio_cms_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+  <footer class="entry-footer">
+    <?php vansunstudio_cms_entry_footer(); ?>
+  </footer>
+</article>
